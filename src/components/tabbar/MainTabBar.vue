f1463431ad6a237d98a30d22a6b1bc3600d8c645
Saige <1499340752@qq.com>
2020-04-28 10:42:37 +0800

    tabbar

---
+++ src/components/tabbar/MainTabBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="#d4237a">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home-active.svg" alt="">
+          <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="#d4237a">
+        <img slot="item-icon" src="~assets/img/tabbar/menu.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/menu-active.svg" alt="">
+          <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="#d4237a">
+        <img slot="item-icon" src="~assets/img/tabbar/cart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/cart-active.svg" alt="">
+          <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="#d4237a">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile-active.svg" alt="">
+          <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+<script>
+import TabBar from 'components/tabbar/TabBar'
+import TabBarItem from 'components/tabbar/TabbarItem'
+export default {
+    name: 'MainTabBar',
+    components: {
+        TabBar, TabBarItem
+    }
+}
+</script>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>主页</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: "Home"
+}
+</script>
